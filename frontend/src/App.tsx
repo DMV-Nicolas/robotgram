3a0877ed1cb2f53ce8261e2fc85f8dd2da7b42ac
DMV-Nicolas <dmvnicolas@gmail.com>
2023-12-27 18:29:36 -0500

    react app was changed for use SWC and the structure of a post was added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,12 @@
-import { useEffect, useState } from 'react'
 import './App.css'
+import { PostCard } from './PostCard'
+
 
 function App() {
-  const [data, setData] = useState<any[]>([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('http://localhost:5000/users?offset=0&limit=1000')
-      const jsonData = await response.json()
-      setData(jsonData)
-    }
-    fetchData()
-
-  }, [])
-
   return (
     <>
       <h1>Robotgram</h1>
-      <div>
-        <ul>
-          {data.map(user => (
-            <li key={user.id}>
-              {user.username} ({user.gender})
-            </li>
-          ))}
-        </ul>
-      </div>
+      <PostCard />
     </>
   )
 }
