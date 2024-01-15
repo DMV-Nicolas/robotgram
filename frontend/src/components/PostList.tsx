41ffb8c5a5f296feb10c0a20da010c109a7565e6
DMV-Nicolas <dmvnicolas@gmail.com>
2024-01-15 09:03:15 -0500

    list of posts component was added

--- frontend/src/components/PostList.tsx
+++ frontend/src/components/PostList.tsx
@@ -1,11 +1,12 @@
 import { usePosts } from "../hooks/usePosts"
 import { Post } from "./Post"
+import "./PostList.css"
 
 export function PostList() {
     const { posts } = usePosts()
 
     return (
-        <ul>
+        <ul className="postList">
             {
                 posts.map((post) => (
                     <li key={post.id}>
