685b497c9a9ed8b58177f34c03e8e9b7df73394d
DMV-Nicolas <dmvnicolas@gmail.com>
2024-01-20 17:23:27 -0500

    basic navbar was added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,39 @@
-import { Robotgram } from "./Icons";
+import { Create, EmptyHeart, Explore, Home, Search } from "./Icons";
+import "./Navbar.css"
+
+type NavbarLiItemParams = {
+    icon: JSX.Element
+    text: string
+}
+
+function NavbarLiItem({ icon }: NavbarLiItemParams) {
+
+}
 
 export function Navbar() {
-    // TODO: Create the navbar
     return (
-        <nav>
-            <Robotgram />
+        <nav className="navbar">
+            <h1 className="title">Robotgram</h1>
+            <ul>
+                <li>
+                    <a href="/"><Home /> <p>Home</p></a>
+                </li>
+                <li>
+                    <a href="/search"><Search /> <p>Search</p></a>
+                </li>
+                <li>
+                    <a href="/explore"><Explore /> <p>Explore</p></a>
+                </li>
+                <li>
+                    <a href="/notifications"><EmptyHeart /> <p>Notifications</p></a>
+                </li>
+                <li>
+                    <a href="/create"><Create /> <p>Create</p></a>
+                </li>
+                <li>
+                    <a href="/profile"><img className="avatar" src="https://avatars.githubusercontent.com/u/69326361?v=4" alt="avatar" /> Profile</a>
+                </li>
+            </ul>
         </nav>
     )
 }
