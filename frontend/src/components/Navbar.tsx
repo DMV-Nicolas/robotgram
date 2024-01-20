3023a4f1ba27a681f809710ccabf63fb1acf3fa9
DMV-Nicolas <dmvnicolas@gmail.com>
2024-01-20 17:56:21 -0500

    bottom link was added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,38 +1,60 @@
-import { Create, EmptyHeart, Explore, Home, Search } from "./Icons";
+import { Blank, Create, EmptyHeart, Explore, Github, Home, Search } from "./Icons";
 import "./Navbar.css"
-
-type NavbarLiItemParams = {
-    icon: JSX.Element
-    text: string
-}
-
-function NavbarLiItem({ icon }: NavbarLiItemParams) {
-
-}
 
 export function Navbar() {
     return (
         <nav className="navbar">
             <h1 className="title">Robotgram</h1>
             <ul>
-                <li>
-                    <a href="/"><Home /> <p>Home</p></a>
-                </li>
-                <li>
-                    <a href="/search"><Search /> <p>Search</p></a>
-                </li>
-                <li>
-                    <a href="/explore"><Explore /> <p>Explore</p></a>
-                </li>
-                <li>
-                    <a href="/notifications"><EmptyHeart /> <p>Notifications</p></a>
-                </li>
-                <li>
-                    <a href="/create"><Create /> <p>Create</p></a>
-                </li>
-                <li>
-                    <a href="/profile"><img className="avatar" src="https://avatars.githubusercontent.com/u/69326361?v=4" alt="avatar" /> Profile</a>
-                </li>
+                <div>
+                    <li>
+                        <a className="navbarLink" href="/">
+                            <Home />
+                            <p>Home</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="navbarLink" href="/search">
+                            <Search />
+                            <p>Search</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="navbarLink" href="/explore">
+                            <Explore />
+                            <p>Explore</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="navbarLink" href="/notifications">
+                            <EmptyHeart />
+                            <p>Notifications</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="navbarLink" href="/create">
+                            <Create />
+                            <p>Create</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="navbarLink" href="/profile">
+                            <img className="avatar" src="https://avatars.githubusercontent.com/u/69326361?v=4" alt="avatar" />
+                            <p>Profile</p>
+                        </a>
+                    </li>
+                </div>
+                <div>
+                    <li>
+                        <a className="navbarLink" href="https://github.com/DMV-Nicolas/robotgram" target="_blank">
+                            <Github />
+                            <p>Github</p>
+                            <span className="navbarLinkBlank">
+                                <Blank />
+                            </span>
+                        </a>
+                    </li>
+                </div>
             </ul>
         </nav>
     )
