58de1acd351a88145b37b21013d331f2e83f101f
DMV-Nicolas <dmvnicolas@gmail.com>
2024-02-02 14:41:18 -0500

    NotFound page was added

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -4,12 +4,13 @@ import './NotFound.css'
 export function NotFound() {
   return (
     <div className='notFound'>
-      <h1>Not found (404)</h1>
-      <p>
-        The link you selected may not work or the page may have been removed.
-        <Link to="/"> Return to Robotgram</Link>
-      </p>
-      <img src="" alt="" />
+      <div className='content'>
+        <h1 className='title'>404</h1>
+        <p className='text'>
+          The link you selected may not work or the page may have been removed.
+          <Link to="/"> Return to Robotgram</Link>
+        </p>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,7 +1,5 @@
 import { NotFound } from '../components/NotFound'
 
 export function NotFoundPage() {
-  return (
-    <NotFound />
-  )
+  return <NotFound />
 }
