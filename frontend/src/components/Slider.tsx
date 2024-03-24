b14a4d150a50e630a581e8c0011ff90b84c6570c
DMV-Nicolas <dmvnicolas@gmail.com>
2024-03-23 19:21:56 -0500

    resize observer for slider was added

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -1,4 +1,4 @@
-import { createRef, useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './Slider.css'
 
 interface Props {
@@ -10,7 +10,7 @@ interface Props {
 export function Slider({ id, username, images }: Props) {
   const [slide, setSlide] = useState(0)
   const [sliderHeight, setSliderHeight] = useState(0)
-  const sliderRef = createRef<HTMLDivElement>()
+  const sliderRef = useRef<HTMLDivElement>(null)
 
   const prevSlide = (): void => {
     if (slide > 0) setSlide(slide - 1)
@@ -21,17 +21,31 @@ export function Slider({ id, username, images }: Props) {
   }
 
   useEffect(() => {
-    if (sliderRef.current instanceof HTMLDivElement) {
+    if (!(sliderRef.current instanceof HTMLDivElement)) return
+
+    const resizeObserver = new ResizeObserver(() => {
+      if (!(sliderRef.current instanceof HTMLDivElement)) {
+        console.log(sliderRef.current)
+        return
+      }
+
       const image = new Image()
       image.src = images[0]
-      const sliderWidth = sliderRef.current.clientWidth
+      const sliderWidth = sliderRef.current.offsetWidth
 
       image.onload = () => {
         const height = image.height / (image.width / sliderWidth)
         setSliderHeight(height)
+        console.log('Hola')
       }
+    })
+
+    resizeObserver.observe(sliderRef.current)
+
+    return () => {
+      resizeObserver.disconnect()
     }
-  }, [])
+  }, [sliderRef.current])
 
   return (
     <div className="slider" ref={sliderRef}>
